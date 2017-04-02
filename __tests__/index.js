2b2bfdb65a666c9fe8b4e792a410f30c9274d57c
Andrea Carraro <andrea.carraro@mosaicoon.com>
2017-04-02 16:42:24 +0200

    Reformat tests

--- __tests__/index.js
+++ __tests__/index.js
@@ -11,9 +11,9 @@ beforeEach(() => {
 describe('createCachedSelector', () => {
   it('Should use the same cached selector when resolver function returns the same string', () => {
     const cachedSelector = createCachedSelector(
-        memoizedFunction,
+      memoizedFunction,
     )(
-        (arg1, arg2) => arg2,   // Resolver
+      (arg1, arg2) => arg2,   // Resolver
     );
     const firstCall = cachedSelector('foo', 'bar');
     const secondCallWithSameResolver = cachedSelector('foo', 'bar');
@@ -23,9 +23,9 @@ describe('createCachedSelector', () => {
 
   it('Should create 2 different selectors when resolver function returns different strings', () => {
     const cachedSelector = createCachedSelector(
-        memoizedFunction,
+      memoizedFunction,
     )(
-        (arg1, arg2) => arg2,   // Resolver
+      (arg1, arg2) => arg2,   // Resolver
     );
     const firstCallResult = cachedSelector('foo', 'bar');
     const secondCallWithDifferentResolver = cachedSelector('foo', 'moo');
@@ -35,9 +35,9 @@ describe('createCachedSelector', () => {
 
   it('Should return "undefined" if provided resolver does not return a string', () => {
     const cachedSelector = createCachedSelector(
-        memoizedFunction,
+      memoizedFunction,
     )(
-        () => {},   // Resolver
+      () => {},   // Resolver
     );
     const firstCallResult = cachedSelector('foo', 'bar');
 
@@ -47,9 +47,9 @@ describe('createCachedSelector', () => {
 
   it('Should allow resolver function to return keys of type number', () => {
     const cachedSelector = createCachedSelector(
-        memoizedFunction,
+      memoizedFunction,
     )(
-        (arg1, arg2) => arg2,   // Resolver
+      (arg1, arg2) => arg2,   // Resolver
     );
     const firstCall = cachedSelector('foo', 1);
     const secondCallWithSameResolver = cachedSelector('foo', 1);
@@ -57,7 +57,7 @@ describe('createCachedSelector', () => {
     expect(memoizedFunction.mock.calls.length).toBe(1);
   });
 
-  it('should expose underlying reselect selector for a cache key with getMatchingSelector', () => {
+  it('Should expose underlying reselect selector for a cache key with getMatchingSelector', () => {
     const cachedSelector = createCachedSelector(
       memoizedFunction,
     )(

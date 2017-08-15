0093c7c51bcf9118213f101ddb6a95ed1a38a910
Andrea Carraro <me@andreacarraro.it>
2017-08-15 02:06:21 +0200

    Fix fifo cache

--- __tests__/fifoCache.js
+++ __tests__/fifoCache.js
@@ -2,13 +2,62 @@ import { FifoCacheObject } from '../index';
 
 const selectorFn = () => {};
 
+function fillCache(cache, entries = []) {
+  entries.map(entry => cache.set(entry, entry));
+  return cache;
+}
+
 describe('FifoCacheObject', () => {
   it('Should return cached value', () => {
-    const cache = new FifoCacheObject({ cacheSize: 10 });
-    cache.set('foo', selectorFn);
+    const cache = new FifoCacheObject({ cacheSize: 5 });
 
-    const value = cache.get('foo');
+    const actual = () => {};
+    cache.set('foo', actual);
+    const expected = cache.get('foo');
 
-    expect(value).toBe(selectorFn);
+    expect(actual).toBe(expected);
+  });
+
+  it('Should limit cache queue by removing the first item', () => {
+    const cache = new FifoCacheObject({ cacheSize: 5 });
+
+    cache.set(0, 0);
+    expect(cache.get(0)).toBe(0);
+
+    const newEntries = [1, 2, 3, 4, 5];
+    fillCache(cache, newEntries)
+
+    expect(cache.get(0)).toBe(undefined);
+
+    newEntries.map(entry => {
+      expect(cache.get(entry)).toBe(entry);
+    });
+  });
+
+  it('Should remove a single item', () => {
+    const cache = new FifoCacheObject({ cacheSize: 5 });
+
+    const newEntries = [1, 2, 3, 4, 5];
+    fillCache(cache, newEntries);
+    cache.remove(3);
+
+    expect(cache.get(3)).toBe(undefined);
+
+    [1, 2, 4, 5].map( entry => {
+      expect(cache.get(entry)).toBe(entry);
+    });
+  });
+
+  it('Should clear the cache', () => {
+    const cache = new FifoCacheObject({ cacheSize: 5 });
+
+    const newEntries = [1, 2, 3, 4, 5];
+    fillCache(cache, newEntries);
+
+    cache.clear();
+
+    newEntries.map( entry => {
+      expect(cache.get(entry)).toBe(undefined);
+    });
   });
 });
--- index.js
+++ index.js
@@ -27,16 +27,23 @@ export class FifoCacheObject {
   }
   set(key, selectorFn) {
     this._cache[key] = selectorFn;
+    this._cacheOrdering.push(key);
+
     if (this._cacheOrdering.length > this._cacheSize) {
-        this._cacheOrdering.shift();
-        delete this._cache[key];
+      const earliest = this._cacheOrdering.shift();
+      delete this._cache[earliest];
     }
   }
   get(key) {
     return this._cache[key];
   }
   remove(key) {
-    delete this._cache[key];
+    const index = this._cacheOrdering.indexOf(key);
+
+    if (index > -1) {
+      this._cacheOrdering.splice(index, 1);
+      delete this._cache[key];
+    }
   }
   clear() {
     this._cache = {};

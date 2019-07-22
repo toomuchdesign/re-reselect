94d5ed73b61306abd64ca648f2cf70baf911b200
Andrea Carraro <me@andreacarraro.it>
2019-07-22 17:58:41 +0200

    Remove deprecated cache exports

--- src/cache/__tests__/deprecated/FifoCacheObject.js
+++
@@ -1,7 +0,0 @@
-import {FifoCacheObject as CacheObject} from '../../../../src/index';
-
-describe('FifoCacheObject (deprecated)', () => {
-  it('returns "FifoObjectCache" class', () => {
-    expect(CacheObject.name).toBe('FifoObjectCache');
-  });
-});

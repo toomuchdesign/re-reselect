c51b9a5a14c3e99e4af27b2c400c0d0f2e36c6d6
Andrea Carraro <me@andreacarraro.it>
2023-12-07 19:12:41 +0100

    test: convert tests to ts (part I)

---
+++ src/__tests__/createStructuredCachedSelector.test.ts
@@ -0,0 +1,36 @@
+import {createStructuredCachedSelector} from '../index';
+
+describe('createStructuredCachedSelector', () => {
+  it('returns expected cachedSelector', () => {
+    type State = {
+      a: string;
+      b: number;
+    };
+    // Example taken from https://github.com/reduxjs/reselect/tree/v4.0.0#createstructuredselectorinputselectors-selectorcreator--createselector
+    const mySelectorA = (state: State) => state.a;
+    const mySelectorB = (state: State) => state.b;
+    const keySelector = (state: State) => state.a;
+
+    const structuredSelector = createStructuredCachedSelector({
+      x: mySelectorA,
+      y: mySelectorB,
+    })(keySelector);
+
+    expect(structuredSelector({a: 'moo', b: 101})).toEqual({
+      x: 'moo',
+      y: 101,
+    });
+
+    // dependencies
+    expect(structuredSelector.dependencies).toEqual([mySelectorA, mySelectorB]);
+
+    // Dependencies
+    const expectedResultFunc = (a: string, b: number) => ({x: a, y: b});
+    expect(structuredSelector.resultFunc('foo', 10)).toEqual(
+      expectedResultFunc('foo', 10)
+    );
+
+    // keySelector
+    expect(structuredSelector.keySelector).toBe(keySelector);
+  });
+});
